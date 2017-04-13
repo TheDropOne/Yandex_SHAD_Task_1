b5f5321c4ce812671c49a56a3b67efef741bf448
TheDropOne <TheDropOne@gmail.com>
2017-04-14 00:42:21 +0300

    Initial commit

---
+++ app/src/main/java/by/thedrop/yandex_shad_task_1/WelcomePageActivity.java
@@ -0,0 +1,13 @@
+package by.thedrop.yandex_shad_task_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_page);
+    }
+}
